9cc9263b84d840ee3575cb90b33efc1ec4560cfa
Yuri Kevin <yurikevin1405@gmail.com>
2024-02-13 14:45:05 -0300

    Atributos da classe e método de guardar mensagens em messageList.

--- src/app/chat/principal/principal.component.ts
+++ src/app/chat/principal/principal.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ChatService } from '../chat.service';
 import { Message } from '../message';
+import { isNgTemplate } from '@angular/compiler';
 
 @Component({
   selector: 'app-principal',
@@ -8,19 +9,36 @@ import { Message } from '../message';
   styleUrls: ['./principal.component.css']
 })
 export class PrincipalComponent implements OnInit{
+  text: string = '';
+  userId: number = 1;
+  messageList: any[] = [];
+
   constructor(private chatService:ChatService){
 
   }
 
   ngOnInit(): void {
       this.chatService.joinRoom("ABC");
+      this.receivedMessage();
   }
 
   sendMessage(){
+    this.text = this.text.replace(/\n/g, '');
     const message = {
-      message: 'hola',
-      user:'1'
+      message: this.text,
+      user: 1
     } as Message
     this.chatService.sendMessage("ABC", message)
+    this.text = '';
+    console.log(message.user === this.userId ? 'your' : 'friend')
+  }
+
+  receivedMessage(){
+      this.chatService.getMessageSubject().subscribe((messages: any) =>{
+        this.messageList = messages.map((item:Message) => ({
+          ...item,
+          message_user: item.user === this.userId ? 'your' : 'friend'
+        }))
+      });
   }
 }
