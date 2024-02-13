5b922ed9d99a00031ead351ab8c63466bf66e08c
Yuri Kevin <yurikevin1405@gmail.com>
2024-02-12 23:15:53 -0300

    Método de enviar mensagem.

--- src/app/chat/principal/principal.component.ts
+++ src/app/chat/principal/principal.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChatService } from '../chat.service';
+import { Message } from '../message';
 
 @Component({
   selector: 'app-principal',
   templateUrl: './principal.component.html',
   styleUrls: ['./principal.component.css']
 })
-export class PrincipalComponent {
+export class PrincipalComponent implements OnInit{
+  constructor(private chatService:ChatService){
 
+  }
+
+  ngOnInit(): void {
+      this.chatService.joinRoom("ABC");
+  }
+
+  sendMessage(){
+    const message = {
+      message: 'hola',
+      user:'1'
+    } as Message
+    this.chatService.sendMessage("ABC", message)
+  }
 }
