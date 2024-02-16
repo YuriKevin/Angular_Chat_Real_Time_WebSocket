b60c614517ad3f75f26b9e8de0e048837bb7801e
Yuri Kevin <yurikevin1405@gmail.com>
2024-02-16 18:55:34 -0300

    método para adicionar contato

--- src/app/chat/new/new.component.ts
+++ src/app/chat/new/new.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ChatService } from '../chat.service';
+import { Contact } from '../contact';
 
 @Component({
   selector: 'app-new',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['../principal/principal.component.css','./new.component.css']
 })
 export class NewComponent {
+  nickname!:string;
+  telephone!:number;
 
+  constructor(private chatService:ChatService){
+    
+  }
+
+  addContact(): void {
+    this.chatService.addContact(this.nickname, this.telephone).subscribe({
+      next: (contact: Contact) => {
+        console.log('Contato adicionado com sucesso!');
+        this.chatService.addContactInArray(contact);
+      },
+      error: (error) => {
+        console.error('Erro ao adicionar contato:', error);
+        
+      }
+    });
+  }
 }
