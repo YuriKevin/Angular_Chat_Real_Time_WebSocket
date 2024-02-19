f42221dd5a2a4ec08346fe4ccbc4297ccc490877
Yuri Kevin <yurikevin1405@gmail.com>
2024-02-19 20:42:49 -0300

    Classe warning component com os atributos e métodos necessários para manipular os feedbacks pro usuário

--- src/app/chat/warning/warning.component.ts
+++ src/app/chat/warning/warning.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-warning',
@@ -6,4 +6,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./warning.component.css']
 })
 export class WarningComponent {
+  show: boolean = false;
+  warning!: string;
+  loading!: boolean;
+  options!: boolean;
+  ok!:boolean;
+  write!:boolean;
+  text!:string;
+
+  @Output() confirmAction: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @Output() textOutput: EventEmitter<string> = new EventEmitter<string>();
+
+  updateValues(warning: string, loading: boolean, options: boolean, ok:boolean, write:boolean): void {
+    this.show = true;
+    this.warning = warning;
+    this.loading = loading;
+    this.options = options;
+    this.ok = ok;
+    this.write = write;
+  }
+
+  confirm() {
+    this.confirmAction.emit(true);
+    this.close();
+  }
+
+  cancel() {
+    this.confirmAction.emit(false);
+    this.close();
+  }
+
+  close(){
+    this.show = false;
+  }
+
+  writeEmit() {
+    this.textOutput.emit(this.text);
+    this.text = '';
+    this.close();
+    this.updateValues('Carregando alteração.', true, false, false, false);
+  }
+  
 }
