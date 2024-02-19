82828b822265f2d85abcc5978a2619cd04a6bb1d
Yuri Kevin <yurikevin1405@gmail.com>
2024-02-19 20:39:42 -0300

    Método para adicionar contato refeito com os feedbacks possíveis através de warning.

--- src/app/chat/new/new.component.ts
+++ src/app/chat/new/new.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ChatService } from '../chat.service';
 import { Contact } from '../contact';
 import { User } from '../user';
 import { ActivatedRoute } from '@angular/router';
+import { WarningComponent } from '../warning/warning.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new',
@@ -15,38 +17,70 @@ export class NewComponent implements OnInit{
   contacts!:Contact[];
   user!:User;
   idRoute!:number;
+  @ViewChild(WarningComponent) warningComponent!: WarningComponent;
 
-  constructor(private chatService:ChatService, private route: ActivatedRoute){
+  constructor(private chatService:ChatService, private route: ActivatedRoute, private router:Router){
     
   }
 
   ngOnInit(): void {
+    if(!this.user){//primeiro if pra quando o ngOnInit for iniciado pela segunda ou demais vezes
+      this.user = this.chatService.getUser();
+      if(!this.user){
+        this.router.navigate(['/login']);
+      }
+    }
       this.idRoute = this.route.snapshot.params['id'];
       if(this.idRoute){
         this.telephone=this.idRoute;
       }
       this.contacts = this.chatService.getContacts();
-      this.chatService.getUser();
+      
+  }
+  add(){
+    this.chatService.addContact(this.nickname, this.telephone).subscribe({
+      next: (contact: Contact) => {
+        this.chatService.addContactInArray(contact, false);
+        this.warningComponent.updateValues('Contato adicionado com sucesso', false, false, true, false);
+        this.nickname = "";
+        this.telephone = 0;
+      },
+      error: (error) => {
+        this.warningComponent.updateValues(error, false, false, true, false);
+      }
+    });
   }
 
-  addContact(): void {
-      const indexContact = this.contacts.findIndex(contact => contact.telephone == this.telephone);
-      if(indexContact == -1){
-      this.chatService.addContact(this.nickname, this.telephone).subscribe({
-        next: (contact: Contact) => {
-          this.chatService.addContactInArray(contact, false);
-        },
-        error: (error) => {
-          console.error('Erro ao adicionar contato:', error);
-          
+    addContact(): void {
+      if(this.nickname.length===0){
+        this.warningComponent.updateValues('Você precisa digitar um nome', false, false, true, false);
+        return;
+      }
+      const numberString: string = this.telephone.toString();
+      if (numberString.length !== 11) {
+        this.warningComponent.updateValues('O número precisa ter 11 dígitos', false, false, true, false);
+        return;
+      }
+
+      if(this.telephone == this.user.telephone){
+        this.warningComponent.updateValues('você não pode adicionar a si mesmo', false, false, true, false);
+      }
+      else if(this.contacts == null){
+        this.add();
+      }
+      else{
+        const indexContact = this.contacts.findIndex(contact => contact.telephone == this.telephone);
+        if(indexContact == -1){
+          this.add();
         }
-      });
-    }
-    else if(this.telephone == this.user.telephone){
-      console.log("você não pode adicionar a si mesmo.");
-    }
-    else{
-      console.log("o contato já existe na sua agenda ("+this.contacts[indexContact].nickname+").");
+        else{
+          if(this.contacts[indexContact].id==0){
+            this.add();
+          }
+          else{
+            this.warningComponent.updateValues('o contato já existe na sua agenda', false, false, true, false);
+            }
+        }
     }
   }
 }
