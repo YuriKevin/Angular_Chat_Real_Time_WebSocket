505bbed36462d8a206ced8a65116f764fd4e338b
Yuri Kevin <yurikevin1405@gmail.com>
2024-02-19 20:38:41 -0300

    método de login atualizado com verificações para feedback.

--- src/app/chat/login/login.component.ts
+++ src/app/chat/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ChatService } from '../chat.service';
+import { WarningComponent } from '../warning/warning.component';
 
 @Component({
   selector: 'app-login',
@@ -7,14 +8,27 @@ import { ChatService } from '../chat.service';
   styleUrls: ['../principal/principal.component.css','./login.component.css']
 })
 export class LoginComponent {
-    telephone!:number
+    telephone!:number;
+    password!:string;
+    @ViewChild(WarningComponent) warningComponent!: WarningComponent;
 
     constructor(private chatService:ChatService){
 
     }
 
-    login(){
-      this.chatService.login(this.telephone);
+    login() {
+      const numberString: string = this.telephone.toString();
+      if (numberString.length !== 11) {
+        this.warningComponent.updateValues('O número precisa ter 11 dígitos', false, false, true, false);
+        return;
+      }
+      this.chatService.login(this.telephone, this.password).subscribe({
+        next: () => {
+          
+        },
+        error: (error) => {
+          this.warningComponent.updateValues(error, false, false, true, false);
+        }
+      });
     }
-
 }
